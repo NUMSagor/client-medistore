870f89003f7c1b11185ed823d96b1bf7b84cbcc6
ab sam <sam749598@gmail.com>
2026-04-09 18:28:23 +0600

    update api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,27 +1,19 @@
 import axios from "axios";
 
-
 const apiClient = axios.create({
-    baseURL:process.env.NEXT_PUBLIC_API_URL || "http://localhost:5050/api",
-    headers:{
-        "Content-Type": "application/json", 
-    },
-    withCredentials: true
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5050/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
 
-
 apiClient.interceptors.request.use((config) => {
-    if(typeof window !== "undefined"){
-        const token = localStorage.getItem("token");
-        if(token){
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config
-    }
-
-    return config;
-},
-(error)=>Promise.reject(error)
-);
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default apiClient;
